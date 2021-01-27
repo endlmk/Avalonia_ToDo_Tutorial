f36ea75620d96ef589151a414ebdb870ad83945b
endlmk <endlmk@users.noreply.github.com>
2021-01-27 23:50:32 +0900

    Add AddItem UI.

---
+++ ToDo/ViewModels/AddItemViewModel.cs
@@ -0,0 +1,7 @@
+namespace ToDo.ViewModels
+{
+    public class AddItemViewModel : ViewModelBase
+    {
+        public string Description { get; set; }
+    }
+}
--- ToDo/ViewModels/MainWindowViewModel.cs
+++ ToDo/ViewModels/MainWindowViewModel.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ReactiveUI;
 using ToDo.Services;
 
 namespace ToDo.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        private ViewModelBase content;
+        
         public MainWindowViewModel(Database db)
         {
-            List = new ToDoListViewModel(db.GetItems());
+            Content = List = new ToDoListViewModel(db.GetItems());
         }
-        
+
+        public ViewModelBase Content
+        {
+            get => content;
+            private set => this.RaiseAndSetIfChanged(ref content, value);
+        }
         public ToDoListViewModel List { get; }
+
+        public void AddItem()
+        {
+            Content = new AddItemViewModel();
+        }
     }
 }
